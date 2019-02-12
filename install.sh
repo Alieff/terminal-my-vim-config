efafd057cc2cb3551b13e17b041b12a1e26dc596
Alief <serdralfs@gmail.com>
2019-02-12 21:39:59 +0700

    fix install path

--- install.sh
+++ install.sh
@@ -1,9 +1,11 @@
 installer_path="$(dirname ${BASH_SOURCE[0]})"
 destination_path="$HOME/.vimrc"
+script_name='vimrc'
 
 
-if [ ! -f "$destination_path" ]
-  cp "$installer_path" "$destination_path"
+
+if [ ! -f "$destination_path" ]; then
+  cp "$installer_path/$script_name" "$destination_path"
 else
   echo ".vimrc already exists, please remove/move manually first"
 fi
