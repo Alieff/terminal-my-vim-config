3258cdd6d64b9ff2a40fbec7e6cd96f19943f8ad
Alief <serdralfs@gmail.com>
2019-07-31 12:02:42 +0700

    docs: improve

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ mkdir -p "$HOME/.vim/undodir"
 # copy the script 
 if [ ! -f "$destination_path" ]; then
   cp "$installer_path/$script_name" "$destination_path"
-  echo ".vimrc installed, now you can restart your vim to check it :)"
+  echo ".vimrc installed, now you can restart your vim to check it :) \n if you want to update, uninstall then reinstall it again"
 else
   echo ".vimrc already exists, please remove/move manually first"
 fi
